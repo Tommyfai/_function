42802d8deb2298ad2149616d8cac01b046d9f855
Tommyfai <tommyfai@gmail.com>
2019-09-17 16:05:53 +0800

    sdfg

--- js/vue-index.js
+++ js/vue-index.js
@@ -2,5 +2,16 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue! - '
+  },
+  methods: {
+    btn: function() {
+      this.message = this.message + 'asdf'
+    }
   }
 })
+
+
+
+
+asdfasdf zsdfsadf asf 
+asdfasdf zsdfsadf asf 
