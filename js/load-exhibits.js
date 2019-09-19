d8edeb55467637cfe3c1d89eec5d01fe083c0042
Tommyfai <tommyfai@gmail.com>
2019-09-19 10:04:20 +0800

    a

--- js/load-exhibits.js
+++ js/load-exhibits.js
@@ -13,50 +13,43 @@ Vue.component('exhibit-list', {
 })
 function sortFunction(a, b) {
   if (a[0] === b[0]) {
-    return 0;
-  }
-  else {
-    return (a[0] < b[0]) ? -1 : 1;
+    return 0
+  } else {
+    return a[0] < b[0] ? -1 : 1
   }
 }
 new Vue({
-  el: "#root",
+  el: '#root',
   data: {
     active: true,
     animaltypes: [],
-    exhibits: [],
+    exhibits: []
   },
-  components: {
-
-  },
+  components: {},
   methods: {
-    sorting: function (_type) {
+    sorting: function(_type) {
       function _sortType(a, b) {
-        if (a.Category < b.Category)
-          return -1;
-        if (a.Category > b.Category)
-          return 1;
-        return 0;
+        if (a.Category < b.Category) return -1
+        if (a.Category > b.Category) return 1
+        return 0
       }
       function _sortPrice(a, b) {
-        if (a.物種分類 < b.物種分類)
-          return -1;
-        if (a.物種分類 > b.物種分類)
-          return 1;
-        return 0;
+        if (a.物種分類 < b.物種分類) return -1
+        if (a.物種分類 > b.物種分類) return 1
+        return 0
       }
-      if (_type === 'name')
-        this.animaltypes.sort(_sortType);
-      if (_type === 'price')
-        this.animaltypes.sort(_sortPrice);
+      if (_type === 'name') this.animaltypes.sort(_sortType)
+      if (_type === 'price') this.animaltypes.sort(_sortPrice)
     }
   },
   mounted() {
-    $.getJSON('../data/exhibits-data.json', function (_data) {
-      this.animaltypes = _data.AnimalTypes;
-      this.exhibits = _data.Exhibits;
-      console.log(_data.AnimalTypes[0].AnimalType + '==')
-    }.bind(this)); +
+    $.getJSON(
+      '../data/exhibits-data.json',
+      function(_data) {
+        this.animaltypes = _data.AnimalTypes
+        this.exhibits = _data.Exhibits
+        console.log(_data.AnimalTypes[0].AnimalType + '==')
+      }.bind(this)
+    )
   }
-
 })
