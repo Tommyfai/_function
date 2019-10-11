e1e684d0bfd4e11c1175e3f25103179431ac13c9
Tommyfai <tommyfai@gmail.com>
2019-10-11 14:34:14 +0800

    asd

--- js/load-google-data.js
+++ js/load-google-data.js
@@ -38,36 +38,70 @@ new Vue({
   },
 
   mounted: function () {
-    var spreadsheetID = "1NcYqzx397HXmFwWhc5ro9N4-tzcbFKtqmLd6kIghh8Y";
-    var url = 'https://spreadsheets.google.com/feeds/cells/' + spreadsheetID + '/1/public/full?alt=json';
+    var id = "1NcYqzx397HXmFwWhc5ro9N4-tzcbFKtqmLd6kIghh8Y";
+    var sheet = 1;
+    var url = 'https://spreadsheets.google.com/feeds/cells/' + id + '/' + sheet + '/public/values?alt=json';
+    // var url = 'https://spreadsheets.google.com/feeds/list/' + id + '/' + sheet + '/public/values?alt=json';
+    // var url = 'https://spreadsheets.google.com/feeds/cells/' + spreadsheetID + '/1/public/full?alt=json';
+    var showRows = true;
+    var showColumns = true;
+    // var query = '&q=漁農自然護理署';
+    var query = '';
+    var useIntegers = true;
     $.getJSON(
       url,
-      // 'data/exhibits-data.json',
-      // 1NcYqzx397HXmFwWhc5ro9N4-tzcbFKtqmLd6kIghh8Y
-      // 'https://spreadsheets.google.com/feeds/lists/2PACX-1vSpLxw6AYhh8QMrzRP-AZKtlXvmC-iwkeIhYSxhdtxbsj-b89eSk4QB7SMBcqK-KlL_fQI9LS9Tliwo/public/values?alt=json',
-      // 'https://spreadsheets.google.com/feeds/cells/1g4FBktkm7al3ZkDI8LuFXuztTqK4nY-eUYMLep6BRuw/1/public/full?alt=json',
-      // 'https://spreadsheets.google.com/feeds/cells/1NcYqzx397HXmFwWhc5ro9N4-tzcbFKtqmLd6kIghh8Y/1/public/full?alt=json',
-      // 'https://sheetsu.com/apis/v1.0su/9d057488f931',
-      // 'https://spreadsheets.google.com/feeds/cells/1NcYqzx397HXmFwWhc5ro9N4-tzcbFKtqmLd6kIghh8Y/0/od6/public/values?alt=json',
-      // 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSpLxw6AYhh8QMrzRP-AZKtlXvmC-iwkeIhYSxhdtxbsj-b89eSk4QB7SMBcqK-KlL_fQI9LS9Tliwo/pubhtml',
-      function (_data) {
-        var entry = _data.feed.entry;
-        $(entry).each(function () {
-          // Column names are name, age, etc.
-          // $('.results').prepend('<h2>' + this.gsx$name.$t + '</h2><p>' + this.gsx$age.$t + '</p>');
-          // console.log(this)
-          // console.log(this.content.$t)
-          // console.log(this.content)
-          // console.log(this.id.$t);
-          // console.log(this.title.$t);
-          // console.log(this.content.$t);
-          // ['gsx$content']['$t']
-
-          console.log(this['gsx$content']['$t']);
-        });
+      function (data) {
+        var responseObj = {};
+        var rows = [];
+        var columns = {};
+        if (data && data.feed && data.feed.entry) {
+          // console.log(data.feed.entry);
+          for (var i = 0; i < data.feed.entry.length; i++) {
+            var entry = data.feed.entry[i];
+            var keys = Object.keys(entry);
+            var newRow = {};
+            var queried = false;
+            for (var j = 0; j < keys.length; j++) {
+              var gsxCheck = keys[j].indexOf('gs$');
+              // var gsxCheck = keys[j].indexOf('gsx$');
+              // console.log(keys[j]);
+              if (gsxCheck > -1) {
+                var key = keys[j];
+                var name = key.substring(4);
+                var content = entry[key];
+                var value = content.$t;
+                if (value.toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                  queried = true;
+                }
+                if (useIntegers === true && !isNaN(value)) {
+                  value = Number(value);
+                }
+                newRow[name] = value;
+                if (queried === true) {
+                  if (!columns.hasOwnProperty(name)) {
+                    columns[name] = [];
+                    columns[name].push(value);
+                  } else {
+                    columns[name].push(value);
+                  }
+                }
+              }
+            }
+            if (queried === true) {
+              rows.push(newRow);
+            }
+          }
+        }
+        if (showColumns === true) {
+          responseObj['columns'] = columns;
+        }
+        if (showRows === true) {
+          responseObj['rows'] = rows;
+        }
+        console.log(responseObj['rows']);
+        //   return res.status(200).json(responseObj);
       }.bind(this)
     )
-
   },
   created: function () {
     //this.result = this.exhibits;
